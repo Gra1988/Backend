ac2802064cda40b2994394f591d4d2bb56e90eac
Gra1988 <95895591+Gra1988@users.noreply.github.com>
2023-02-05 13:22:14 -0300

    prueba link

--- src/main/java/com/portfolio/gsc/Controller/ProyectoController.java
+++ src/main/java/com/portfolio/gsc/Controller/ProyectoController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://app-frontgsc.web.app")
+@CrossOrigin(origins = "https://app-frontgsc.web.app/")
 public class ProyectoController {
     @Autowired IProyectoService iproyectoService;
  
